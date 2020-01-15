cdf81a0f3996b27aa0ab1c735d7e0d65acd8c54b
Nanar Boursalian <nanar.boursalian01@gmail.com>
2020-01-15 15:23:34 -0800

    Initial Commit

---
+++ BigTips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BigTips
+//
+//  Created by Nanar Boursalian on 1/15/20.
+//  Copyright © 2020 NanarBoursalian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
